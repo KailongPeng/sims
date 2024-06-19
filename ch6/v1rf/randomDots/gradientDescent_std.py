b9ef755b586f81df5568d7ccca3313099108b7e7
KailongPeng <kailongpeng001@gmail.com>
2024-06-19 13:59:45 -0400

    gradient descent std

--- ch6/v1rf/randomDots/gradientDescent_std.py
+++ ch6/v1rf/randomDots/gradientDescent_std.py
@@ -1,28 +1,3 @@
-"""
-假代码
-
-对于n=21个点，首先把这些点随机均匀平铺在一个0到1的二维平面上，然后把这21个点随机向任意方向移动一点点，这个随机移动的距离被scalling factor lambda控制。
-
-计算objective：
-对于输入的n个点，计算出移动前和移动后分别的两个距离矩阵 n x n 。这两个距离矩阵中的上半矩阵元素数量为n*(n-1)/2个。移动前的n*(n-1)/2个元素记为m1，移动后的n*(n-1)/2个元素记为m2.
-将m1值的相反数作为x轴，将m2-m1的值作为y轴，画一个散点图。
-对于这个散点图，获取x轴上面的最小的n*(n-1)/2/3个元素记为 noChange, 获取x轴上面的最大的n*(n-1)/2/3个元素记为 integration, 剩下的n*(n-1)/2/3个元素记为differentiation.
-对于noChange,计算这n*(n-1)/2/3个元素的y轴的与0相比的t test 的 t 值的绝对值加上 t test的p value, 记为 obj_noChange.
-对于differentiation,计算这n*(n-1)/2/3个元素的y轴的与0相比的t值加上 t test的p value, 记为 obj_differentiation.
-对于integration,计算这n*(n-1)/2/3个元素的y轴的与0相比的t值的相反数加上 t test的p value, 记为 obj_integration.
-
-总体的objective = obj_noChange + obj_differentiation + obj_integration.
-
-现在对于这21个点, 轮流对他们进行随机的移动, 这个随机移动的距离被scalling factor lambda控制。每一个移动之后, 计算一次objective, 如果objective变小,则保留这次移动, 如果objective不变或者变大,则放弃这次移动.
-
-最后, 展示最开始没有移动的21个点在二维平面上的位置,以及最终完成所有的移动之后的21个点在二维平面上的位置.
-
-与此同时,也要展示最开始和结束的时候的objective的x轴和y轴的散点图
-
-还要记录然后最终画出训练过程的loss curve
-
-"""
-
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 from scipy.stats import ttest_1samp
@@ -42,7 +17,7 @@ n = 20
 # 定义控制点随机移动距离的缩放因子
 lambda_factor = 0.01  # 示例缩放因子
 # 定义优化迭代的次数
-iterations = 100  # 优化迭代次数
+iterations = 50  # 优化迭代次数
 
 # 在0到1的二维平面上随机均匀分布n个点
 points = np.random.rand(n, 2)
@@ -62,17 +37,17 @@ initial_dist_matrix = calculate_distance_matrix(points)
 # 定义一个函数，用于计算目标函数和散点图数据
 def calculate_objective_and_plot_data(initial_dist_matrix, new_dist_matrix):
     # 计算移动前后的距离矩阵的上三角元素
-    m1 = initial_dist_matrix[np.triu_indices(n, k=1)]
-    m2 = new_dist_matrix[np.triu_indices(n, k=1)]
+    m1_distanceMatrix = initial_dist_matrix[np.triu_indices(n, k=1)]
+    m2_distanceMatrix = new_dist_matrix[np.triu_indices(n, k=1)]
 
     # 将移动前的元素取相反数作为x轴，移动后的元素与移动前的元素差作为y轴
-    x = -m1
-    y = m2 - m1
+    x_coactivation = - m1_distanceMatrix  # m 越大, 越differentiation, 越少的co-activation
+    y_integration = - (m2_distanceMatrix - m1_distanceMatrix)  # m 越大, 越differentiation, 因此
 
     # 根据x轴的值进行排序
-    sorted_indices = np.argsort(x)
-    x_sorted = x[sorted_indices]
-    y_sorted = y[sorted_indices]
+    sorted_indices = np.argsort(x_coactivation)
+    x_sorted = x_coactivation[sorted_indices]
+    y_sorted = y_integration[sorted_indices]
 
     # 将排序后的x轴值分为三部分：noChange, differentiation, integration
     third = len(x_sorted) // 3
@@ -91,7 +66,7 @@ def calculate_objective_and_plot_data(initial_dist_matrix, new_dist_matrix):
 
     # 计算总体目标函数值
     objective = obj_noChange + obj_differentiation + obj_integration
-    return objective, x, y, mean_noChange, std_noChange, mean_differentiation, std_differentiation, mean_integration, std_integration
+    return objective, x_coactivation, y_integration, mean_noChange, std_noChange, mean_differentiation, std_differentiation, mean_integration, std_integration
 
 
 def calculate_gradient(points, initial_dist_matrix):
@@ -134,19 +109,27 @@ def calculate_gradient(points, initial_dist_matrix):
             grad_std_integration /= np.linalg.norm(grad_std_integration) + 1e-8
 
             # 设置权重
+            # weights = {
+            #     'mean_noChange': 1,
+            #     'std_noChange': 1,
+            #     'mean_differentiation': 1,
+            #     'std_differentiation': 1,
+            #     'mean_integration': 1,
+            #     'std_integration': 1
+            # }
             weights = {
-                'mean_noChange': 1,
-                'std_noChange': 1,
+                'mean_noChange': 0,
+                'std_noChange': 0,
                 'mean_differentiation': 1,
-                'std_differentiation': 1,
-                'mean_integration': 1,
-                'std_integration': 1
+                'std_differentiation': 0,
+                'mean_integration': 0,
+                'std_integration': 0
             }
 
             # 加和归一化后的梯度
             gradient[i, j] = (
                     grad_mean_noChange * weights['mean_noChange']
-                    + grad_std_noChange * weights['std_noChange']  # 注意 std_noChange 的符号为正，因为我们希望它更小
+                    + grad_std_noChange * weights['std_noChange']
                     + grad_mean_differentiation * weights['mean_differentiation']
                     + grad_std_differentiation * weights['std_differentiation']
                     - grad_mean_integration * weights['mean_integration']  # 注意 mean_integration 的符号为负，因为我们希望它更大
@@ -193,28 +176,15 @@ std_integration_list = [initial_std_integration]
 for _ in tqdm(range(iterations)):
     gradient = calculate_gradient(best_points, initial_dist_matrix)  # 计算梯度
     # new_points = best_points - lambda_factor * gradient  # 沿负梯度方向更新点的位置
-    learningRate = 1/100
-    new_points = best_points - gradient/np.max(np.abs(gradient)) * learningRate  # 沿负梯度方向更新点的位置
+    learningRate = 1e-8 #0.000001
+    # new_points = best_points - gradient / np.max(np.abs(gradient)) * learningRate  # 沿负梯度方向更新点的位置
+    new_points = best_points - gradient * learningRate  # 沿负梯度方向更新点的位置
     new_dist_matrix = calculate_distance_matrix(new_points)  # 计算新的距离矩阵
     (new_objective, new_x, new_y,
      mean_noChange, std_noChange,
      mean_differentiation, std_differentiation,
      mean_integration, std_integration) = calculate_objective_and_plot_data(initial_dist_matrix, new_dist_matrix)  # 计算新的目标函数值和散点图数据
 
-    # 如果新的目标函数值小于最佳目标函数值，则更新最佳点集和最佳目标函数值
-    # criteria = new_objective < best_objective
-    # criteria = (np.abs(mean_noChange) <= np.abs(best_mean_noChange) and
-    #             std_noChange <= best_std_noChange and
-    #             mean_differentiation <= best_mean_differentiation and
-    #             std_differentiation <= best_std_differentiation and
-    #             mean_integration >= best_mean_integration and
-    #             std_integration <= best_std_integration)
-    # criteria = (np.abs(mean_noChange) <= np.abs(best_mean_noChange) and
-    #             mean_differentiation <= best_mean_differentiation and
-    #             mean_integration >= best_mean_integration)
-    # criteria = mean_differentiation <= best_mean_differentiation
-    # criteria = mean_integration >= best_mean_integration
-    # if criteria:
     best_points = new_points.copy()
     best_objective = new_objective
     best_mean_noChange = mean_noChange
