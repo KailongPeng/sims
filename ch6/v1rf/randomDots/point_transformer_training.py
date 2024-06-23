36992f96c725b73186818e9d3b3a35ef401844a1
KailongPeng <kailongpeng001@gmail.com>
2024-06-23 17:59:22 -0400

    use result of gradientDescent_pcit.py to train NN

--- ch6/v1rf/randomDots/point_transformer_training.py
+++ ch6/v1rf/randomDots/point_transformer_training.py
@@ -29,29 +29,29 @@ class PointTransformer(nn.Module):
         x = self.fc3(x)
         return x
 
-# Early stopping class
-class EarlyStopping:
-    def __init__(self, patience=10, min_delta=0):
-        self.patience = patience
-        self.min_delta = min_delta
-        self.best_loss = None
-        self.counter = 0
-
-    def __call__(self, loss):
-        if self.best_loss is None:
-            self.best_loss = loss
-            return False
-
-        if loss < self.best_loss - self.min_delta:
-            self.best_loss = loss
-            self.counter = 0
-        else:
-            self.counter += 1
-
-        return self.counter >= self.patience
+# # Early stopping class
+# class EarlyStopping:
+#     def __init__(self, patience=10, min_delta=0):
+#         self.patience = patience
+#         self.min_delta = min_delta
+#         self.best_loss = None
+#         self.counter = 0
+#
+#     def __call__(self, loss):
+#         if self.best_loss is None:
+#             self.best_loss = loss
+#             return False
+#
+#         if loss < self.best_loss - self.min_delta:
+#             self.best_loss = loss
+#             self.counter = 0
+#         else:
+#             self.counter += 1
+#
+#         return self.counter >= self.patience
 
 # Function to train the model and record weights and activations
-def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
+def train_model(best_points_history, patience=10, min_delta=0, max_epochs=10000):
     initial_points = best_points_history[0]  # shape: (40, 2)
     best_points_history = torch.tensor(best_points_history, dtype=torch.float32)
     initial_points = torch.tensor(initial_points, dtype=torch.float32)
@@ -59,6 +59,7 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
     model = PointTransformer()
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs, eta_min=0)
 
     # Storage for weight changes and activations
     weight_changes = []
@@ -66,7 +67,7 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
 
     losses = {}
     for t in range(1, best_points_history.shape[0]):
-        early_stopping = EarlyStopping(patience=patience, min_delta=min_delta)
+        # early_stopping = EarlyStopping(patience=patience, min_delta=min_delta)
         for epoch in tqdm(range(max_epochs)):
             optimizer.zero_grad()
             outputs = model(initial_points)
@@ -77,6 +78,7 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
             losses[t].append(loss.item())
             loss.backward()
             optimizer.step()
+            scheduler.step()
 
             # Record the weight changes of the third layer (fc3)
             weight_change = model.fc3.weight.grad.clone().detach().numpy()
@@ -88,7 +90,8 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
             activations.append(activation)
 
             # Visualize training outputs and targets
-            if epoch % (max_epochs // 10) == 0:  # Plot every few epochs
+            display_interval = 1 #  这个参数控制了每多少个epoch就会画一次图，当等于1的时候，只显示最后一次的图，当等于2的时候，显示最后一次和最中间一次的图，以此类推
+            if (1+epoch) % (max_epochs // display_interval) == 0:  # Plot every few epochs
                 plt.figure(figsize=(10, 5))
                 plt.scatter(outputs[:, 0].detach().numpy(), outputs[:, 1].detach().numpy(), color='blue', label='Predicted Points')
                 plt.scatter(target[:, 0].detach().numpy(), target[:, 1].detach().numpy(), color='red', label='Target Points')
@@ -96,10 +99,11 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
                 plt.legend()
                 plt.show()
 
-            # Check early stopping
-            if early_stopping(loss.item()):
-                print(f"Early stopping at epoch {epoch} for time point {t}")
-                break
+            # # Check early stopping
+            # if early_stopping(loss.item()):
+            #     print(f"Early stopping at epoch {epoch} for time point {t}")
+            #     break
+
     # Plot the loss curves
     for t in range(1, best_points_history.shape[0]):
         plt.figure(figsize=(10, 5))
@@ -114,7 +118,7 @@ def train_model(best_points_history, patience=10, min_delta=0, max_epochs=1000):
 
 best_points_history = np.asarray(np.load('./result/best_points_history.npy'))  # (np.array(best_points_history).shape=(101, 40, 2))。
 best_points_history = best_points_history[0:3]
-model, weight_changes, activations = train_model(best_points_history, patience=10, min_delta=1e-20, max_epochs=10000)
+model, weight_changes, activations = train_model(best_points_history, patience=100, min_delta=1e-20, max_epochs=10000)
 
 weight_changes = np.array(weight_changes)
 activations = np.array(activations)
